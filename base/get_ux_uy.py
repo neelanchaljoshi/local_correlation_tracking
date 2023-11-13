38d4f738c54ff2af8c1d6ace5a3e94340f96ec6a
Neelanchal Joshi <neelanchaljoshi@gmail.com>
2023-11-13 17:32:55 +0100

    iterative lct for ux, uy

--- base/get_ux_uy.py
+++ base/get_ux_uy.py
@@ -7,47 +7,43 @@ Created on Thu Mar  2 14:04:13 2023
 """
 
 import numpy as np
+from scipy.ndimage.interpolation import shift
+from scipy.linalg import lstsq
 
 
-def get_ux_uy_ellipsoid(clat, clng, ccf_av, patch_size, R_sun, pixel_size, cadence):
-	ym, xm = np.where(ccf_av==ccf_av[1:-1, 1:-1].max())
-	xmax = xm[0]
-	ymax = ym[0]
+def get_ux_uy(ccf_av, grid_len = 5, patch_size = 168, pixel_size = 0.03, R_sun = 696, ntry = 3):
+	# Finds the ux and uy from the ccf_av
+	dx_tot = 0
+	dy_tot = 0
 
-	f00 = ccf_av[ymax, xmax]
-	f10 = ccf_av[ymax, xmax+1]
-	f11 = ccf_av[ymax+1, xmax+1]
-	f01 = ccf_av[ymax+1, xmax]
-	f_11 = ccf_av[ymax+1, xmax-1]
-	f_10 = ccf_av[ymax, xmax-1]
-	f_1_1 = ccf_av[ymax-1, xmax-1]
-	f0_1 = ccf_av[ymax-1, xmax]
-	f1_1 = ccf_av[ymax-1, xmax+1]
+	assert grid_len%2 == 1
 
+	for n in range(ntry):
+		ym, xm = np.where(ccf_av==ccf_av[grid_len:-grid_len, grid_len:-grid_len].max())
+		xmax = xm[0]
+		ymax = ym[0]
 
+		fxy = ccf_av[ymax-grid_len//2:ymax+(grid_len+1)//2, xmax-grid_len//2:xmax+(grid_len+1)//2]
+		val_arr = fxy.flatten()
 
-	fxy = [[f00], [f10], [f11], [f01], [f_11], [f_10], [f_1_1], [f0_1], [f1_1]]
+		x = y = np.linspace(-grid_len//2+1, grid_len//2, grid_len)
 
-	A = np.array([[0,0,0,0,0,1], [1,0,1,0,0,1], [1,1,1,1,1,1], [0,1,0,1,0,1], [1,1,-1,1,-1,1], [1,0,-1,0,0,1], [1,1,-1,-1,1,1], [0,1,0,-1,0,1], [1,1,1,-1,-1,1]])
-	B = np.matmul(A.T, A)
-	Binv = np.linalg.inv(B)
-	complete = np.matmul(Binv, A.T)
+		coeff_arr = []
+		for j, i in [[_i, _j] for _i in x for _j in y]:
+			coeff_arr.append([i**2, j**2, i, j, i*j, 1])
 
+		coeff_array = np.array(coeff_arr)
+		p, res, rnk, s = lstsq(coeff_array, val_arr)
 
-	A = np.matmul(complete, fxy)
+		a,b,c,d,e,f = p
 
-	a,b,c,d,e,f = A
+		ypar = ((e*c)-(2*a*d))/((4*a*b-(e**2)))
+		xpar= (-e*ypar - c)/(2*a)
 
-	a,b,c,d,e = a[0], b[0], c[0], d[0], e[0]
+		del_x = xmax  - patch_size//2 + xpar
+		del_y = ymax  - patch_size//2 + ypar
 
-	ypar = ((e*c)-(2*a*d))/((4*a*b-(e**2)))
-	xpar= (-e*ypar - c)/(2*a)
+		ccf_av = shift(ccf_av,[-del_y, -del_x], mode = 'reflect')
 
-	del_x = xmax  - patch_size//2 + xpar
-	del_y = ymax  - patch_size//2 + ypar
-
-	ux = (R_sun * pixel_size * (np.pi/180) * del_x)/(cadence) * 1e6
-	uy = (R_sun * del_y * pixel_size * (np.pi/180))/(cadence) * 1e6
-
-
-	return del_x, del_y, ux, uy
+		dx_tot = dx_tot + del_x
+		dy_tot = dy_tot + del_y
