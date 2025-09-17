32a039997c5a22a168dc4133620620440e7b7b22
Neelanchal Joshi <neelanchaljoshi@gmail.com>
2025-09-17 10:50:17 +0200

    changed the config file to get obs_vr for hmi.v_45s and main to get it only for the year 2019

--- get_hmi_keys/config.py
+++ get_hmi_keys/config.py
@@ -1,4 +1,4 @@
-cadence = 720  # in seconds
+cadence = 45  # in seconds
 seriesname = f'hmi.v_{cadence}s'  # HMI Continuum Intensity, 45s cadence
 outdir = f'/scratch/seismo/joshin/pipeline-test/IterativeLCT/hmi.v_{cadence}s'
 
@@ -7,6 +7,7 @@ QbitsPass = 0b00000000000000000000000000000000
 KeyList = [
     ('t_rec', bytes),
     ('t_obs', bytes),
+    ('obs_vr', float),
     ('quality', str),
     ('crpix1', float),
     ('crpix2', float),
--- get_hmi_keys/main.py
+++ get_hmi_keys/main.py
@@ -3,7 +3,7 @@ from process import process_year
 
 if __name__ == "__main__":
     T = [datetime.now()]
-    for yr in range(2010, 2025):  # Change range as needed
+    for yr in range(2019, 2020):  # Change range as needed
         process_year(yr)
         T.append(datetime.now())
         print(f"Total time for {yr}: {T[-1] - T[-2]}")
