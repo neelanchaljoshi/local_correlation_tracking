d77c08d5cf6b6af91c595ceb07b5500cee592043
Neelanchal Joshi <neelanchaljoshi@gmail.com>
2025-09-29 10:38:17 +0200

    plots to check extent of granulation from ccfs and variance

---
+++ plotting_scripts/figure_ccf_widths.py
@@ -0,0 +1,92 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
+plt.rcParams['font.family'] = 'sans-serif'
+from astropy.io import fits
+import h5py
+
+# %%
+# Load the data
+f = h5py.File('/data/seismo/joshin/pipeline-test/local_correlation_tracking/pmi/diff_rot/data/2018_dspan_360_dstep_30_dt_45_ccf_width_test_5deg_gran_4k.hdf5')
+# f = h5py.File('/data/seismo/joshin/pipeline-test/local_correlation_tracking/pmi/diff_rot/data/2018_dspan_360_dstep_30_dt_45_ccf_widths_5deg_mag_4k.hdf5')
+ccf_width_x_gran = f['ccf_width_x'][:]
+ccf_width_y_gran = f['ccf_width_y'][:]
+lons = f['longitude'][:]
+lats = f['latitude'][:]
+
+f = h5py.File('/data/seismo/joshin/pipeline-test/local_correlation_tracking/pmi/diff_rot/data/2018_dspan_360_dstep_120_dt_45_ccf_widths_5deg_mag_4k.hdf5')
+ccf_width_x_mag = f['ccf_width_x'][:]
+ccf_width_y_mag = f['ccf_width_y'][:]
+
+# %%
+# Change the width at each latitude from degrees to Mm on the sun
+R_sun = 696340.0  # in km
+lat_rad = np.radians(lats)
+deg_to_km = (np.pi / 180) * R_sun * np.cos(lat_rad)  # km per degree
+ccf_width_x_gran = ccf_width_x_gran * deg_to_km[:, np.newaxis] / 1000.0  # Convert to Mm
+ccf_width_y_gran = ccf_width_y_gran * deg_to_km[:, np.newaxis] / 1000.0  # Convert to Mm
+ccf_width_x_mag = ccf_width_x_mag * deg_to_km[:, np.newaxis] / 1000.0  # Convert to Mm
+ccf_width_y_mag = ccf_width_y_mag * deg_to_km[:, np.newaxis] / 1000.0  # Convert to Mm
+# %%
+# Plot the data
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm
+
+# Replace NaN or Inf values in the data arrays
+ccf_width_x_gran = np.nan_to_num(ccf_width_x_gran, nan=0.0, posinf=0.0, neginf=0.0)
+ccf_width_y_gran = np.nan_to_num(ccf_width_y_gran, nan=0.0, posinf=0.0, neginf=0.0)
+ccf_width_x_mag = np.nan_to_num(ccf_width_x_mag, nan=0.0, posinf=0.0, neginf=0.0)
+ccf_width_y_mag = np.nan_to_num(ccf_width_y_mag, nan=0.0, posinf=0.0, neginf=0.0)
+
+# Define the custom colormap for granulation-based data
+gran_extra_colors = ['yellow', 'orange', 'red']  # Example extra colors
+gran_colors = [
+    (0, 0, 0, 1),  # Black at -1
+    (1, 1, 1, 1),  # White at 0
+    (0, 1, 0, 1)   # Green at 2
+] + [plt.cm.viridis(i) for i in np.linspace(0, 1, len(gran_extra_colors))]
+gran_cmap = LinearSegmentedColormap.from_list('gran_cmap', gran_colors)
+
+# Define the custom colormap for magnetogram-based data
+mag_extra_colors = ['purple', 'pink', 'brown']  # Example extra colors
+mag_colors = [
+    (0, 0, 0, 1),  # Black at -1
+    (1, 1, 1, 1),  # White at 0
+    (0, 1, 1, 1),  # Cyan at 2.5 (midpoint of 0-5)
+    (0, 0, 1, 1)   # Blue at 5
+] + [plt.cm.plasma(i) for i in np.linspace(0, 1, len(mag_extra_colors))]
+mag_cmap = LinearSegmentedColormap.from_list('mag_cmap', mag_colors)
+
+# Define the boundaries for the colormaps (ensure no NaN or Inf in bounds)
+gran_bounds = [-1, 0.001, 2] + list(np.linspace(2, 10, len(gran_extra_colors) + 1)[1:])
+mag_bounds = [-1, 0.001, 5] + list(np.linspace(5, 10, len(mag_extra_colors) + 1)[1:])
+
+gran_norm = BoundaryNorm(gran_bounds, gran_cmap.N)
+mag_norm = BoundaryNorm(mag_bounds, mag_cmap.N)
+
+# Plot the data
+fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+
+# Granulation-based plots
+c1 = axs[0, 0].pcolormesh(lons, lats, ccf_width_x_mag[0], shading='auto', cmap=mag_cmap, norm=mag_norm)
+fig.colorbar(c1, ax=axs[0, 0], orientation='vertical', label = 'Width (Mm)')
+axs[0, 0].set_title('CCF Width X (Magnetogram-based LCT)')
+
+c2 = axs[0, 1].pcolormesh(lons, lats, ccf_width_y_mag[0], shading='auto', cmap=mag_cmap, norm=mag_norm)
+fig.colorbar(c2, ax=axs[0, 1], orientation='vertical', label = 'Width (Mm)')
+axs[0, 1].set_title('CCF Width Y (Magnetogram-based LCT)')
+
+# Magnetogram-based plots
+c3 = axs[1, 0].pcolormesh(lons, lats, ccf_width_x_gran[0], shading='auto', cmap=gran_cmap, norm=gran_norm)
+fig.colorbar(c3, ax=axs[1, 0], orientation='vertical', label = 'Width (Mm)')
+axs[1, 0].set_title('CCF Width X (Granulation-based LCT)')
+
+c4 = axs[1, 1].pcolormesh(lons, lats, ccf_width_y_gran[0], shading='auto', cmap=gran_cmap, norm=gran_norm)
+fig.colorbar(c4, ax=axs[1, 1], orientation='vertical', label = 'Width (Mm)')
+axs[1, 1].set_title('CCF Width Y (Granulation-based LCT)')
+
+plt.tight_layout()
+plt.show()
+# %%
--- plotting_scripts/figure_variance.py
+++ plotting_scripts/figure_variance.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 12})
 plt.rcParams['font.family'] = 'sans-serif'
+from astropy.io import fits
 
 # %%
 # Load flow data
@@ -10,8 +11,22 @@ uphi_mag = np.load('/data/seismo/joshin/pipeline-test/paper_lct/processed_data/u
 uthe_mag = np.load('/data/seismo/joshin/pipeline-test/paper_lct/processed_data/utheta_hmi_m_720s_dt_1h_processed.npy')
 uphi_gran = np.load('/data/seismo/joshin/pipeline-test/paper_lct/processed_data/uphi_hmi_ic_45s_processed.npy')
 uthe_gran = np.load('/data/seismo/joshin/pipeline-test/paper_lct/processed_data/utheta_hmi_ic_45s_processed.npy')
+inp= '/scratch/seismo/mandowara/HMIRDA05'
+rdv= np.load(inp+'/metadata.npz')
+uphi_rda = fits.getdata(inp+'/BBux_stony_intp.fits')
+uthe_rda = fits.getdata(inp+'/BBuy_stony_intp.fits')
+uphi_rda = uphi_rda[:, 0, :, :]
+uthe_rda = uthe_rda[:, 0, :, :]
+t  = rdv['t_mid_dec']
+
 # %%
 
+t_rec = np.load('/data/seismo/joshin/pipeline-test/local_correlation_tracking/data/t_rec.npy')
+# index of 2019-01-01 00:00:00
+start_index = np.where(t_rec == np.array(['2018.11.20_00:00:00_TAI'], dtype = 'S32'))[0][0]
+print("Start index for 2018-12-01 is ", start_index)
+end_index = np.where(t_rec == np.array(['2019.01.01_00:00:00_TAI'], dtype = 'S32'))[0][0]
+print("End index for 2019-01-01 is ", end_index)
 
 def calculate_variance(flow_array):
     """
@@ -26,10 +41,11 @@ def calculate_variance(flow_array):
     return np.nanvar(flow_array, axis=0)
 # %%
 # Calculate variance for both components
-uphi_mag_variance = calculate_variance(uphi_mag)
-uthe_mag_variance = calculate_variance(uthe_mag)
-uphi_gran_variance = calculate_variance(uphi_gran)
-uthe_gran_variance = calculate_variance(uthe_gran)
+uphi_mag_variance = calculate_variance(uphi_mag[start_index:end_index])
+uthe_mag_variance = calculate_variance(uthe_mag[start_index:end_index])
+uphi_gran_variance = calculate_variance(uphi_gran[start_index:end_index])
+uthe_gran_variance = calculate_variance(uthe_gran[start_index:end_index])
+uphi_rda
 
 # %%
 lon = np.linspace(-90, 90, uphi_mag.shape[2])
@@ -49,5 +65,19 @@ c4 = axs[1, 1].pcolormesh(lon, lat, uthe_gran_variance, shading='auto', cmap='je
 fig.colorbar(c4, ax=axs[1, 1], orientation='vertical')
 axs[1, 1].set_title('Variance of $u_{\\theta}$ (Granulation-based LCT)')
 plt.tight_layout()
+
+# %%
+# cut through the central meridian
+central_lon_index = lon.size // 2
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.semilogy(lat, np.sqrt(uphi_mag_variance[:, central_lon_index]), label='$u_{\\phi}$ (Magnetogram-based LCT)', color='blue')
+ax.semilogy(lat, np.sqrt(uthe_mag_variance[:, central_lon_index]), label='$u_{\\theta}$ (Magnetogram-based LCT)', color='orange')
+ax.semilogy(lat, np.sqrt(uphi_gran_variance[:, central_lon_index]), label='$u_{\\phi}$ (Granulation-based LCT)', color='green')
+ax.semilogy(lat, np.sqrt(uthe_gran_variance[:, central_lon_index]), label='$u_{\\theta}$ (Granulation-based LCT)', color='red')
+ax.set_xlabel('Latitude (degrees)')
+ax.set_ylabel('Std Dev (m/s)')
+ax.set_title('Variance Cut Through Central Meridian (Longitude = 0°)')
+ax.set_ylim(-10, 1000)
+ax.legend()
 
 # %%
